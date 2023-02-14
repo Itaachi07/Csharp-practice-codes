e7e355a33977b2f92dad9a444f1ca0c45fb7b5b8
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-14 22:24:36 +0530

    ref and out keyword

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void reffun(out int i)
+    {
+        i =50;
+       // Console.WriteLine(i);
+    } 
+    static void Main(string[] args)
+    {
+        int r;
+
+      //Console.WriteLine(r);
+      reffun(out r);    
+      Console.WriteLine(r);    
+    }
+
+  }
+}
